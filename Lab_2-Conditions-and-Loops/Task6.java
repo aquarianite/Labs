addb7904c1df1abde7138d985cda05f3d846a64f
Jamie Pearl <80820050+aquarianite@users.noreply.github.com>
2024-09-22 20:02:45 +0200

    Create Task6.java

---
+++ Lab_2-Conditions-and-Loops/Task6.java
@@ -0,0 +1,6 @@
+public class task6 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 999; i++); because here it stands; it will be fulfilled
+{ System.out.println("Hello"); } // and only then will Hello be printed
+    }
+}
